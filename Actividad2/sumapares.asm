51c8e5e07992d1572e106fbbda04ff70d506dba1
aisak <isaac.pineda@upb.edu.co>
2024-08-02 11:36:56 -0500

    actividad 2

---
+++ Actividad2/sumapares.asm
@@ -0,0 +1,34 @@
+@1
+M=0
+@0
+D=M
+@17
+M=D
+@16
+M=0
+//inicio del loop
+(LOOP)
+@16
+M=M+1
+M=M+1
+//prepara la condicion 
+D=D-1
+@LOOP
+//condicion del salto
+D;JGT
+// pasar el valor a la ram 1
+@16
+D=M
+
+@1
+M=D
+
+
+
+
+
+
+
+
+
+
