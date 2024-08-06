befd80c90075f629afc996588379554cb840fc87
isaacrisi <isaacpinedamunera@gmail.com>
2024-08-06 18:29:21 -0500

    terminando los ejercicios

--- Actividad2/sumapares.asm
+++ Actividad2/sumapares.asm
@@ -1,27 +1,34 @@
-@1
-M=0
-@0
-D=M
-@17
-M=D
-@16
-M=0
-//inicio del loop
-(LOOP)
-@16
-M=M+1
-M=M+1
-//prepara la condicion 
-D=D-1
-@LOOP
-//condicion del salto
-D;JGT
-// pasar el valor a la ram 1
-@16
-D=M
+@0         
+D=M        
+@END       
+D;JEQ      
 
+@1         
+M=0
+@0         
+D=M        
+@17       
+M=D
+@16        
+M=0
+
+// Inicio del loop
+(LOOP)
+@16        
+M=M+1
+M=M+1
+
+// Prepara la condición de salida
+D=D-1     
+@LOOP      
+D;JGT      
+
+@16        
+D=M
 @1
 M=D
+
+(END)      
 
 
 
